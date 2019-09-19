754a4759d1e211ecd8c87690ef3cd4b25dd0950b
Michael Babozhko <goodiny777@gmail.com>
2019-09-19 20:22:41 +0300

    Initial commit Webview clien for https://www.spadream.co.il/ With push notification

---
+++ app/src/main/java/il/co/spadream/spadream/AppClass.kt
@@ -0,0 +1,29 @@
+package il.co.spadream.spadream
+
+import android.app.Application
+
+import com.onesignal.OSNotificationOpenResult
+import com.onesignal.OneSignal
+
+class AppClass : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+        OneSignal.startInit(this)
+            .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+            .unsubscribeWhenNotificationsAreDisabled(true)
+            .setNotificationOpenedHandler {
+                val additionalData = it.notification.payload.additionalData
+                if (additionalData!=null){
+                    link = additionalData.getString("link")
+                }
+            }
+            .init()
+    }
+
+    companion object{
+        var link:String? = null
+    }
+}
+
---
+++ app/src/main/java/il/co/spadream/spadream/MainActivity.kt
@@ -0,0 +1,109 @@
+package il.co.spadream.spadream
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var context: Context
+    lateinit var webView: WebView
+    val mainUrl = "https://www.spadream.co.il/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setPointer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (AppClass.link != null) {
+            webView.loadUrl(mainUrl)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            AlertDialog.Builder(context)
+                .setTitle("כבר יוצא")
+                .setMessage("האם אתה בטוח?")
+                .setNegativeButton("לא") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setPositiveButton("כן") { _, _ ->
+                    super.onBackPressed()
+                }
+                .create().show()
+        }
+
+    }
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setPointer() {
+        context = this
+
+        webView = findViewById(R.id.webView)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility = View.GONE
+
+                if (AppClass.link != null) {
+                    webView.loadUrl(AppClass.link)
+                }
+                if (webView.url != mainUrl) {
+                    ivBack.visibility = View.VISIBLE
+                }else{
+                    ivBack.visibility = View.GONE
+
+                }
+            }
+        }
+        webView.settings.javaScriptEnabled = true
+        webView.settings.useWideViewPort = true
+        webView.loadUrl(mainUrl)
+
+        ivBack.setOnClickListener {
+            if (AppClass.link != null) {
+                webView.loadUrl(mainUrl)
+                AppClass.link = null
+            }
+
+            onBackPressed() }
+    }
+}
